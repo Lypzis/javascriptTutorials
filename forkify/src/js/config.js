aacc9531df4c77c52174f2d789b060137358cca2
Lypzis <victorvp2008@hotmail.com>
2019-04-20 22:48:33 -0300

    Minor update

--- forkify/src/js/config.js
+++ forkify/src/js/config.js
@@ -3,7 +3,7 @@
 
 // server proxy
 // cross origin test with cors-anywhere, since the cors is not handled by the owners of this API
-export const proxy = ''; //https://crossorigin.me/   https://cors-anywhere.herokuapp.com/ or just use CorsE browser plugin
+export const proxy = 'https://cors-anywhere.herokuapp.com/'; //https://crossorigin.me/   https://cors-anywhere.herokuapp.com/ or just use CorsE browser plugin
 // API key
 export const key = 'e3f80f041ebcfd75be7d082002a156b1';
 
